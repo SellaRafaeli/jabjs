b5d36ec34520ab95b8fd44b9fe1fcdfa192b6972
Sella Rafaeli <sella.rafaeli@gmail.com>
2014-11-21 02:05:54 +0200

    wip

--- demo/jabArr.js
+++ demo/jabArr.js
@@ -1,18 +1,20 @@
 //wip
-
-
 function jabArr(obj) { 
     arrayChangingFuncs = [ "pop", "push", "reverse", "shift", "unshift", "splice", "sort", "filter"];
 }
 
 obj = [10,20,30] 
 
-function jabArr(arr
 arrayChangingFuncs = [ "pop", "push", "reverse", "shift", "unshift", "splice", "sort", "filter"];
-arrayChangingFuncs.forEach(function(funcName){ 
-    var func = arr[funcName];
-    obj[funcName] = function() {
-        func.apply(this, arguments);
-        console.log("invoked "+funcName);
-    }
-});
+
+function jabArr(obj) { 
+    arrayChangingFuncs.forEach(function(funcName){ 
+        var func = arr[funcName];
+        obj[funcName] = function() {
+            var res = func.apply(this, arguments);
+            console.log("invoked "+funcName);
+            return res;
+        }
+    });
+}
+
